4849407edf0376277f1bb90a87dc1833f0cd734d
Sam Deane <sam@elegantchaos.com>
2025-09-11 22:48:51 +0100

    more recipes

--- main.lua
+++ main.lua
@@ -41,10 +41,12 @@ function Module:onStart()
         ["1799107246"] = 100, -- Basic Pipe
         ["3936127019"] = 100, -- Basic Screw
         ["2662317132"] = 10,  -- Basic Combustion Chamber
+        ["1981362643"] = 10,  -- Basic Standard Frame S
         ["994058182"] = 10,   -- Basic Reinforced Frame S
         ["1331181119"] = 10,  -- Basic Hydraulics
         ["1981362536"] = 10,  -- Uncommon Standard Frame M
         ["994058204"] = 10,   -- Basic Reinforced Frame M
+        ["625289720"] = 10,   -- Basic Chemical Container S
     }
 
     local printer = {
