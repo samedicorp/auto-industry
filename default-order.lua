0bc17280534f87a28fcefe30e5377daf1d55b891
Sam Deane <sam@elegantchaos.com>
2025-09-14 18:36:37 +0100

    moved orders to separate file

---
+++ default-order.lua
@@ -0,0 +1,48 @@
+return {
+
+    smelter = {
+        ["511774178"] = 500,  -- Steel
+        ["018262914"] = 500,  -- Al-Fe
+        ["2565702107"] = 500, -- Silumin
+        ["1034957327"] = 500, -- Calcium Reinforced Copper
+    },
+
+    metalwork = {
+        ["2660328728"] = 25,  -- Basic Burner
+        ["1799107246"] = 100, -- Basic Pipe
+        ["3936127019"] = 100, -- Basic Screw
+        ["3936127018"] = 100, -- Uncommon Screw
+        ["2662317132"] = 10,  -- Basic Combustion Chamber
+        ["1981362643"] = 10,  -- Basic Standard Frame S
+        ["994058182"] = 10,   -- Basic Reinforced Frame S
+        ["1331181119"] = 100, -- Basic Hydraulics
+        ["1981362536"] = 10,  -- Uncommon Standard Frame M
+        ["994058204"] = 10,   -- Basic Reinforced Frame M
+        ["625289720"] = 10,   -- Basic Chemical Container S
+    },
+
+    printer = {
+        ["1971447072"] = 10, -- Basic Injector
+        ["466630565"] = 100, -- Basic Fixation
+    },
+
+    chemical = {
+        ["2014531313"] = 500, -- Polycarbonate
+        ["840202984"] = 1000, -- Kergon X5
+    },
+
+    glass = {
+        ["3308209457"] = 100, -- Glass Product
+        ["1942154251"] = 100, -- Advanced Glass Product
+        ["1234754162"] = 100, -- Basic LED
+        ["1234754161"] = 100, -- Uncommon LED
+    },
+
+    electronics = {
+        ["794666749"] = 100, -- Basic Component
+        ["3808417022"] = 50, -- Basic Processor
+        ["3808417021"] = 10, -- Uncommon Processor
+        ["1080827609"] = 1,  -- Uncommon Antenna M
+        ["4186205972"] = 10, -- Basic Power Transformer
+    }
+}
--- main.lua
+++ main.lua
@@ -12,7 +12,8 @@
 local Module = {}
 
 function Module:register(parameters)
-    modula:registerForEvents(self, "onStart", "onStop", "onCheckMachines", "onCommand")
+    modula:registerForEvents(self, "onStart", "onStopping", "onCheckMachines", "onCommand")
+    self.orderName = parameters.orderName or "samedicorp.auto-industry.default-order"
 end
 
 -- ---------------------------------------------------------------------
@@ -29,60 +30,68 @@ function Module:onStart()
     -- Dual Universe item IDs
     -- see http://du-lua.dev/#/items for reference
 
-    local smelter = {
-        ["511774178"] = 100,  -- Steel
-        ["018262914"] = 100,  -- Al-Fe
-        ["2565702107"] = 100, -- Silumin
-        ["1034957327"] = 100, -- Calcium Reinforced Copper
-    }
+    -- local smelter = {
+    --     ["511774178"] = 500,  -- Steel
+    --     ["018262914"] = 500,  -- Al-Fe
+    --     ["2565702107"] = 500, -- Silumin
+    --     ["1034957327"] = 500, -- Calcium Reinforced Copper
+    -- }
 
-    local metalwork = {
-        ["2660328728"] = 25,  -- Basic Burner
-        ["1799107246"] = 100, -- Basic Pipe
-        ["3936127019"] = 100, -- Basic Screw
-        ["2662317132"] = 10,  -- Basic Combustion Chamber
-        ["1981362643"] = 10,  -- Basic Standard Frame S
-        ["994058182"] = 10,   -- Basic Reinforced Frame S
-        ["1331181119"] = 10,  -- Basic Hydraulics
-        ["1981362536"] = 10,  -- Uncommon Standard Frame M
-        ["994058204"] = 10,   -- Basic Reinforced Frame M
-        ["625289720"] = 10,   -- Basic Chemical Container S
-    }
+    -- local metalwork = {
+    --     ["2660328728"] = 25,  -- Basic Burner
+    --     ["1799107246"] = 100, -- Basic Pipe
+    --     ["3936127019"] = 100, -- Basic Screw
+    --     ["3936127018"] = 100, -- Uncommon Screw
+    --     ["2662317132"] = 10,  -- Basic Combustion Chamber
+    --     ["1981362643"] = 10,  -- Basic Standard Frame S
+    --     ["994058182"] = 10,   -- Basic Reinforced Frame S
+    --     ["1331181119"] = 100, -- Basic Hydraulics
+    --     ["1981362536"] = 10,  -- Uncommon Standard Frame M
+    --     ["994058204"] = 10,   -- Basic Reinforced Frame M
+    --     ["625289720"] = 10,   -- Basic Chemical Container S
+    -- }
 
-    local printer = {
-        ["1971447072"] = 10, -- Basic Injector
-        ["466630565"] = 100, -- Basic Fixation
-    }
+    -- local printer = {
+    --     ["1971447072"] = 10, -- Basic Injector
+    --     ["466630565"] = 100, -- Basic Fixation
+    -- }
 
-    local chemical = {
-        ["2014531313"] = 100, -- Polycarbonate
-    }
+    -- local chemical = {
+    --     ["2014531313"] = 500, -- Polycarbonate
+    --     ["840202984"] = 1000, -- Kergon X5
+    -- }
 
-    local glass = {
-        ["1234754162"] = 100, -- Basic LED
-        ["1234754161"] = 100, -- Uncommon LED
-    }
+    -- local glass = {
+    --     ["3308209457"] = 100, -- Glass Product
+    --     ["1942154251"] = 100, -- Advanced Glass Product
+    --     ["1234754162"] = 100, -- Basic LED
+    --     ["1234754161"] = 100, -- Uncommon LED
+    -- }
 
-    local electronics = {
-        ["3808417021"] = 10, -- Uncommon Processor
-        ["1080827609"] = 1,  -- Uncommon Antenna M
-        ["4186205972"] = 10, -- Basic Power Transformer
-    }
+    -- local electronics = {
+    --     ["794666749"] = 100, -- Basic Component
+    --     ["3808417022"] = 50, -- Basic Processor
+    --     ["3808417021"] = 10, -- Uncommon Processor
+    --     ["1080827609"] = 1,  -- Uncommon Antenna M
+    --     ["4186205972"] = 10, -- Basic Power Transformer
+    -- }
 
-    local orders = {}
-    self:addOrders(orders, smelter, "Smelter")
-    self:addOrders(orders, metalwork, "Metalwork")
-    self:addOrders(orders, printer, "Printer")
-    self:addOrders(orders, chemical, "Chemical")
-    self:addOrders(orders, glass, "Glass")
-    self:addOrders(orders, electronics, "Electronics")
+    local order = require(self.orderName)
+    local buildList = {}
+
+    self:addOrder(buildList, order.smelter, "Smelter")
+    self:addOrder(buildList, order.metalwork, "Metalwork")
+    self:addOrder(buildList, order.printer, "Printer")
+    self:addOrder(buildList, order.chemical, "Chemical")
+    self:addOrder(buildList, order.glass, "Glass")
+    self:addOrder(buildList, order.electronics, "Electronics")
 
     local recipes = {}
-    for id, _ in pairs(orders) do
+    for id, _ in pairs(buildList) do
         table.insert(recipes, id)
     end
 
-    self.orders = orders
+    self.buildList = buildList
     self.recipes = recipes
 
     modula:addTimer("onCheckMachines", 1.0)
@@ -92,8 +101,8 @@ function Module:onStart()
     self:restartMachines()
 end
 
-function Module:onStop()
-    debugf("Auto Industry stopped.")
+function Module:onStopping()
+    debugf("Auto Industry stopping.")
 end
 
 function Module:onContainerChanged(container)
@@ -121,16 +130,22 @@ function Module:restartMachine(machine)
         local index = (1 + (machine.index or 0) % #self.recipes)
         machine.index = index
         local recipe = self.recipes[index]
-        local order = self.orders[recipe]
+        local order = self.buildList[recipe]
         if machine:label():find(order.machine) then
             if not machine:isStopped() then
                 machine:stop()
             end
 
             if machine:setRecipe(recipe) == 0 then
+                machine.target = recipe
                 machine:start(order.quantity)
-                debugf("Trying '%s' for %s (%s).", system.getItem(recipe).locDisplayName, machine:name(), machine:label())
             end
+        end
+    elseif machine:isRunning() then
+        if machine.actual ~= machine.target then
+            debugf("Running '%s' for %s (%s).", system.getItem(machine.target).locDisplayName, machine:name(),
+                machine:label())
+            machine.actual = machine.target
         end
     end
 end
@@ -151,9 +166,9 @@ end
 -- ---------------------------------------------------------------------
 
 
-function Module:addOrders(orders, itemsToAdd, type)
+function Module:addOrder(buildList, itemsToAdd, type)
     for id, quantity in pairs(itemsToAdd) do
-        orders[id] = {
+        buildList[id] = {
             quantity = quantity,
             machine = type,
         }
