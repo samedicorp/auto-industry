29095ba80307bce8329351868aca56678d02e90d
Sam Deane <sam@elegantchaos.com>
2025-09-23 23:33:31 +0100

    tweaked order

--- default-order.lua
+++ default-order.lua
@@ -49,9 +49,11 @@ return {
         ["994058182"] = 5,     -- Basic Reinforced Frame S
         ["994058204"] = 10,    -- Basic Reinforced Frame M
         ["994058205"] = 1,     -- Basic Reinforced Frame L
-        ["1981362536"] = 10,   -- Uncommon Standard Frame M
-        ["1331181119"] = 128,  -- Basic Hydraulics
-        ["625289720"] = 10,    -- Basic Chemical Container S
+        ["1981362536"] = 1,    -- Uncommon Standard Frame M
+        ["1331181119"] = 150,  -- Basic Hydraulics
+        ["625289720"] = 2,     -- Basic Chemical Container S
+        ["625289726"] = 1,     -- Basic Chemical Container M
+        -- ["625289727"] = 1,     -- Basic Chemical Container L
         ["1246524879"] = 1,    -- Uncommon Magnet
         ["494825062"] = 1,     -- Basic Mobile Panel L
         ["997368796"] = 1,     -- Basic Robotic Arm M
@@ -61,6 +63,8 @@ return {
     printer = {
         ["1971447072"] = 10, -- Basic Injector
         ["466630565"] = 100, -- Basic Fixation
+        ["184261487"] = 1,   -- Uncommon Screen S
+        ["567008215"] = 1,   -- Uncommon Casing S
     },
 
     chemical = {
@@ -82,12 +86,13 @@ return {
     },
 
     electronics = {
+        ["2872711779"] = 100, -- Basic Connector
         ["1297540454"] = 200, -- Basic Electronics
         ["1297540453"] = 100, -- Uncommon Electronics
         ["794666749"] = 500,  -- Basic Component
         ["3808417022"] = 50,  -- Basic Processor
         ["3808417021"] = 10,  -- Uncommon Processor
-        ["1080827609"] = 1,   -- Uncommon Antenna M
+        -- ["1080827609"] = 1,   -- Uncommon Antenna M
         ["4186205972"] = 10,  -- Basic Power Transformer
         ["1775106618"] = 1,   -- Uncommon Core System M
         -- ["1775106597"] = 1,   -- Uncommon Core System L
